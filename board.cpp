0ae9ff4e31468e4c23f6eb368a99e6483ee8f828
Ian Ferguson <imferguson@alaska.edu>
2019-02-18 17:22:51 -0900

    Added board.

---
+++ board.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Ian Ferguson and Erin Tolman on 2019-02-18.
+//
+
---
+++ board.h
@@ -0,0 +1,8 @@
+//
+// Created by Ian Ferguson and Erin Tolman on 2019-02-18.
+//
+
+#ifndef CS372_BATTLESHIP_BOARD_H
+#define CS372_BATTLESHIP_BOARD_H
+
+#endif //CS372_BATTLESHIP_BOARD_H
