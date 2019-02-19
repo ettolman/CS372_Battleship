5904601e883783ebed1966b3ae40b60239ca8110
Ian Ferguson <imferguson@alaska.edu>
2019-02-18 20:02:40 -0900

    Added an array of ship sizes and began organizing the class.

--- board.h
+++ board.h
@@ -12,9 +12,20 @@
 class Board{
     
 public:
+    //Default Constructor
     Board();
 
+    //printBoard function
+    //Prints board in ASCII
     void printBoard();
+
+    //Ships
+    static const int DESTROYER_SIZE = 2;
+    static const int SUBMARINE_SIZE = 3;
+    static const int FRIGATE_SIZE = 3;
+    static const int BATTLESHIP_SIZE = 4;
+    static const int CARRIER_SIZE = 5;
+    const int SHIP_SIZE[5] = {DESTROYER_SIZE, SUBMARINE_SIZE, FRIGATE_SIZE, BATTLESHIP_SIZE, CARRIER_SIZE };
     
 private:
     std::vector<int> _board;
