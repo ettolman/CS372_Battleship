9fd2a16aeb40d6ab9bd2b178644a816e57a3a7bf
Ian Ferguson <imferguson@alaska.edu>
2019-02-18 16:51:56 -0900

    ReAdded Diary because it didn't push last time, and added main and SFML compatibility for Clion.

---
+++ cmake-build-debug/CMakeFiles/CS372_Battleship.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS372_Battleship.dir/main.cpp.o"
+  "CS372_Battleship.pdb"
+  "CS372_Battleship"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS372_Battleship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
