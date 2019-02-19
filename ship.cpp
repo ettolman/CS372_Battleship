9e5feb1ef54dde7dac11afea40311dfa7eadd692
Ian Ferguson <imferguson@alaska.edu>
2019-02-18 20:41:58 -0900

    Added ship class.

---
+++ ship.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ian Ferguson on 2019-02-18.
+//
+
+#include "ship.h"
---
+++ ship.h
@@ -0,0 +1,8 @@
+//
+// Created by Ian Ferguson on 2019-02-18.
+//
+
+#ifndef CS372_BATTLESHIP_SHIP_H
+#define CS372_BATTLESHIP_SHIP_H
+
+#endif //CS372_BATTLESHIP_SHIP_H
