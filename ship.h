342650cc404af8db5051ad33fab6f66abb07bc70
Ian Ferguson <imferguson@alaska.edu>
2019-02-21 15:08:34 -0900

    Added mutators and accessors for ship.

--- ship.h
+++ ship.h
@@ -5,4 +5,14 @@
 #ifndef CS372_BATTLESHIP_SHIP_H
 #define CS372_BATTLESHIP_SHIP_H
 
+class Ship
+{
+public:
+    void setHealth(int health);
+    int getHealth();
+    void Hit();
+    bool Destroyed();
+private:
+    int _health;
+};
 #endif //CS372_BATTLESHIP_SHIP_H
