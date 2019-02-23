c4adef14227eb0f921aac0fc3bc63bbe3630c130
Erin <eetolman@hotmail.com>
2019-02-22 16:17:35 -0900

    board is now a string vector

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 // Main prints the and controls the game
 int main() {
-    std::vector <int> height{10};
-    std::vector<std::vector<int>> myBoard(10, height);
+    std::vector <std::string> height{10,"."};
+    std::vector<std::vector<std::string>> myBoard(10, height);
     Board board(myBoard);
     board.printBoard();
+    board.placeShip(1,2);
 }
