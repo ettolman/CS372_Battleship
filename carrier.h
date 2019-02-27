e47c33785688a399c12878ad558d1d7f98c97895
Ian Ferguson <imferguson@alaska.edu>
2019-02-26 15:33:44 -0900

    Forgot to add carrier class, so I made one.

---
+++ carrier.h
@@ -0,0 +1,17 @@
+//
+// Created by Ian Ferguson on 2019-02-26.
+//
+
+#ifndef CS372_BATTLESHIP_CARRIER_H
+#define CS372_BATTLESHIP_CARRIER_H
+#include "ship.h"
+
+class Carrier: public Ship
+{
+    Carrier(): Ship()
+    {
+        setHealth(5);
+    }
+};
+
+#endif //CS372_BATTLESHIP_CARRIER_H
